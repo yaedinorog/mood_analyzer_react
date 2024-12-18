2abfc819e5cf168c7a6f6c1de34515cf493126f2
Iamuniicorn <rud.yar@list.ru>
2024-12-19 02:05:49 +0300

    added flexibility

--- src/script.js
+++ src/script.js
@@ -26,18 +26,6 @@ window.addEventListener('scroll', function() {
     }
 });
 
-const modal = document.getElementById('myModal');
-const openModalBtn = document.getElementById('openModalBtn');
-const closeBtn = document.getElementsByClassName('close')[0];
-
-openModalBtn.onclick = function() {
-    modal.style.display = 'block';
-}
-
-closeBtn.onclick = function() {
-    modal.style.display = 'none';
-}
-
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = 'none';
