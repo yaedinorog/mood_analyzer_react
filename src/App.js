1d57161153f4a507f18968d1dc74f5bd3e93892e
Iamuniicorn <rud.yar@list.ru>
2024-12-19 14:35:25 +0300

    fixed script.js

--- src/App.js
+++ src/App.js
@@ -3,54 +3,58 @@ import './style.css';
 import MOOD from './img/MOOD.png'
 import logo from './img/Logo-icon.svg'
 import tg_logo from './img/Telegram App.svg'
-import { useState } from 'react'
-
-const ModalWindow = () => {
-  return (
-    <div id="myModal" class="modal">
-                    <div class="modal-conf-content">
-                        <span class="close">&times;</span>
-                        <h2 class="modal-conf-title">АВТОРИЗАЦИЯ</h2>
-                        <div class="modal-conf-item">
-                            <input class="checkbox" id="checkbox" type="checkbox" />
-                            <p class="modal-conf-text">Я согласен(на) на обработку предоставленных мной персональных данных в соответствии с политикой конфиденциальности сайта.</p>
-                        </div>
-                        <button id="telegramAuthBtn" class="modal-conf-btn">Войти</button>
-                    </div>
-                </div>
-  )
-}
+import React, { useState } from 'react'
 
 const Header = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const openModal = () => {
-      console.log("bebra")
-      setIsModalOpen(true);
-    };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
 
-    const closeModal = () => {
-      setIsModalOpen(false);
-    };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleClickOutside = (e) => {
+    if (e.target.classList.contains("modal")) {
+      closeModal();
+    }
+  };
 
     return (
-      <header class="header" id="header">
-            <div class="container header-container">
-                <div class="logo">
-                    <a href="index.html" class="logo-link">
-                        <img src={logo} alt="logo" class="logo-img" />
-                        <p class="logo-text">Mood</p>
+      <header className="header">
+            <div className="container header-container">
+                <div className="logo">
+                    <a href="index.html" className="logo-link">
+                        <img src={logo} alt="logo" className="logo-img" />
+                        <p className="logo-text">Mood</p>
                     </a>
                 </div>
-                <nav class="nav">
-                    <div class="menu">
-                        <a href="#work" class="menu-item">Как это работает</a>
-                        <a href="#conf" class="menu-item">Конфиденциальность</a>
-                        <a href="#about" class="menu-item">О нас</a>
+                <nav className="nav">
+                    <div className="menu">
+                        <a href="#work" className="menu-item">Как это работает</a>
+                        <a href="#conf" className="menu-item">Конфиденциальность</a>
+                        <a href="#about" className="menu-item">О нас</a>
                     </div>
                 </nav>
-                <button class="btn" id="openModalBtn" onClick={openModal}>Бамб -&gt;</button>
-                {isModalOpen && (<ModalWindow />)}
+                <button className="btn" onClick={openModal}>Бамб -&gt;</button>
+                {isModalOpen && (
+                  <div className="modal" onClick={handleClickOutside}>
+                    <div className="modal-conf-content">
+                        <span className="close" onClick={closeModal}>&times;</span>
+                        <h2 className="modal-conf-title">АВТОРИЗАЦИЯ</h2>
+                        <div className="modal-conf-item">
+                            <input className="checkbox" type="checkbox" />
+                            <p className="modal-conf-text">Я согласен(на) на обработку предоставленных мной персональных данных в соответствии с политикой конфиденциальности сайта.</p>
+                        </div>
+                        <button className="modal-conf-btn" onClick={() => {
+                              const url = 'https://t.me/Mood_auth_bot?start=auth';
+                              window.location.href = url;
+                            }}>Войти</button>
+                    </div>
+                </div>
+              )}
             </div>
         </header>
     )
@@ -58,12 +62,12 @@ const Header = () => {
 
 const MoodText = () => {
   return (
-    <div class="banner">
-      <div class="container banner-container">
-        <div class="banner-title">
-            <img class="banner-title-text" src={MOOD} />
-            <p class="banner-text-left">Используйте наш ИИ для анализа сообщений и сделайте общение более глубоким и искренним.</p>
-            <p class="banner-text-right">Просто нажмите на кнопку "Бамб", чтобы мгновенно получить анализ настроения вашего друга</p>
+    <div className="banner">
+      <div className="container banner-container">
+        <div className="banner-title">
+            <img className="banner-title-text" src={MOOD} />
+            <p className="banner-text-left">Используйте наш ИИ для анализа сообщений и сделайте общение более глубоким и искренним.</p>
+            <p className="banner-text-right">Просто нажмите на кнопку "Бамб", чтобы мгновенно получить анализ настроения вашего друга</p>
         </div>
       </div>
     </div>
@@ -72,29 +76,29 @@ const MoodText = () => {
 
 const MainText = () => {
   return( 
-    <div class="work" id="work">
-                <div class="container work-container">
-                    <h2 class="work-title">Как это работает</h2>
-                    <div class="work-content">
-                        <div class="work-item">
-                            <p class="work-item-title">1. Авторизация через Telegram</p>
-                            <p class="work-item-text">Для начала вам нужно авторизоваться с помощью вашего аккаунта Telegram.</p>
+    <div className="work" id="work">
+                <div className="container work-container">
+                    <h2 className="work-title">Как это работает</h2>
+                    <div className="work-content">
+                        <div className="work-item">
+                            <p className="work-item-title">1. Авторизация через Telegram</p>
+                            <p className="work-item-text">Для начала вам нужно авторизоваться с помощью вашего аккаунта Telegram.</p>
                         </div>
-                        <div class="work-item">
-                            <p class="work-item-title">2. Пригласите друга</p>
-                            <p class="work-item-text">Поделитесь ссылкой на наш сервис с друзьями, чтобы они тоже могли воспользоваться всеми преимуществами.</p>
+                        <div className="work-item">
+                            <p className="work-item-title">2. Пригласите друга</p>
+                            <p className="work-item-text">Поделитесь ссылкой на наш сервис с друзьями, чтобы они тоже могли воспользоваться всеми преимуществами.</p>
                         </div>
-                        <div class="work-item">
-                            <p class="work-item-title">3. Согласие на обработку данных</p>
-                            <p class="work-item-text">Для того чтобы использовать наш сервис, необходимо дать согласие двух сторон на обработку ваших данных.</p>
+                        <div className="work-item">
+                            <p className="work-item-title">3. Согласие на обработку данных</p>
+                            <p className="work-item-text">Для того чтобы использовать наш сервис, необходимо дать согласие двух сторон на обработку ваших данных.</p>
                         </div>
-                        <div class="work-item">
-                            <p class="work-item-title">4. Искусственный интеллект</p>
-                            <p class="work-item-text">Теперь вам доступен искусственный интеллект, который анализирует настроение вашего друга.  Также ИИ предлагает дополнительные функции, такие как подробный анализ настроения и подбор фильмов на основе эмоций.</p>
+                        <div className="work-item">
+                            <p className="work-item-title">4. Искусственный интеллект</p>
+                            <p className="work-item-text">Теперь вам доступен искусственный интеллект, который анализирует настроение вашего друга.  Также ИИ предлагает дополнительные функции, такие как подробный анализ настроения и подбор фильмов на основе эмоций.</p>
                         </div>
-                        <div class="work-item">
-                            <p class="work-item-title">5. Подробный анализ настроения</p>
-                            <p class="work-item-text">Чтобы увидеть подробный анализ настроения вашего друга, нажмите на эмодзи рядом с его иконкой на дашборде. В модальном окне отобразится информация о текущем настроении и рекомендациях по фильмам.</p>
+                        <div className="work-item">
+                            <p className="work-item-title">5. Подробный анализ настроения</p>
+                            <p className="work-item-text">Чтобы увидеть подробный анализ настроения вашего друга, нажмите на эмодзи рядом с его иконкой на дашборде. В модальном окне отобразится информация о текущем настроении и рекомендациях по фильмам.</p>
                         </div>
                     </div>
                 </div>
@@ -104,10 +108,10 @@ const MainText = () => {
 
 const Conf = () => {
   return (
-    <div class="conf" id="conf">
-      <div class="container conf-container">
-        <h2 class="conf-title">Конфиденциальность</h2>
-        <p class="conf-text">
+    <div className="conf" id="conf">
+      <div className="container conf-container">
+        <h2 className="conf-title">Конфиденциальность</h2>
+        <p className="conf-text">
           Мы строго защищаем вашу конфиденциальность. Все данные, которые вы предоставляете, используются
           исключительно для улучшения работы сервиса и для того, чтобы наш искусственный интеллект мог
           предоставлять вам максимальные возможности. Ваши данные не передаются третьим сторонам без
@@ -123,10 +127,10 @@ const Conf = () => {
 
 const About = () => {
   return (
-  <div class="about" id="about">
-    <div class="container about-container">
-      <h2 class="about-title">О нас</h2>
-      <p class="about-text">
+  <div className="about" id="about">
+    <div className="container about-container">
+      <h2 className="about-title">О нас</h2>
+      <p className="about-text">
         Мы — команда, которая создает инновационные решения на основе искусственного интеллекта, чтобы
         улучшить ваши общения. Наш сервис анализирует настроение вашего друга в чате Telegram, изучая
         его сообщения и предоставляя вам полезную информацию о его эмоциях. Это помогает лучше понять
@@ -141,56 +145,42 @@ const About = () => {
 
 const Footer = () => {
   return (
-    <footer class="footer">
-            <div class="container footer-container">
-                <div class="logo">
-                    <a href="index.html" class="logo-link">
-                        <img src={logo} alt="logo" class="logo-img" />
-                        <p class="logo-text">Mood</p>
+    <footer className="footer">
+            <div className="container footer-container">
+                <div className="logo">
+                    <a href="index.html" className="logo-link">
+                        <img src={logo} alt="logo" className="logo-img" />
+                        <p className="logo-text">Mood</p>
                     </a>
                 </div>
-                <div class="footer-content">
-                    <div class="footer-item">
-                        <a href="use.html" class="footer-item-link">Условия пользования</a>
-                        <a href="conf.html" class="footer-item-link">Политика кофидинчиальности</a>
-                        <a href="#" class="footer-item-link">Управление файлами cookies</a>
+                <div className="footer-content">
+                    <div className="footer-item">
+                        <a href="use.html" className="footer-item-link">Условия пользования</a>
+                        <a href="conf.html" className="footer-item-link">Политика кофидинчиальности</a>
+                        <a href="#" className="footer-item-link">Управление файлами cookies</a>
                     </div>
                 </div>
-                <div class="footer-social">
-                    <img src={tg_logo} alt="#" class="social-img" />
+                <div className="footer-social">
+                    <img src={tg_logo} alt="#" className="social-img" />
                 </div>
             </div>
         </footer>
   )
 }
 
-function MainPage() {
+function App() {
   return (
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>MOOD</title>
-        <link rel="stylesheet" href="style.css" />
-    </head>
-    <body>
-        <Header />
-        <main>
-            <MoodText />
-            <MainText />
-            <Conf />
-            <About />
-        </main>
-        <Footer />
-    </body>
-    </html>
+  <>
+    <Header />
+    <main>
+      <MoodText />
+      <MainText />
+      <Conf />
+      <About />
+    </main>
+    <Footer />
+  </>
   )
 }
-function App() {
-  return(
-    <MainPage />
-  );
-}
-
 
 export default App;
